a7487695597dc105ba3ddfca47c1d7fa55eb6ed3
Stack Soumik <sarkarsoumik215@gmail.com>
2024-07-10 20:02:04 +0600

    [update] dotenv file add

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
-export const MONGODB_CONNECTION = "mongodb+srv://soum-ik:frontenddev@cluster0.dunrodk.mongodb.net/asttha_it_solution?retryWrites=true&w=majority&appName=Cluster0";
+export const MONGODB_CONNECTION = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASS}@cluster0.dunrodk.mongodb.net/asttha_it_solution?retryWrites=true&w=majority&appName=Cluster0`;
 export const URL_ENCODED = true;
 export const PORT = 3000; 
