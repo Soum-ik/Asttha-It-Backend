aa8f023ec5c249fb958c3c6fbfc313f2feb6513e
Stack Soumik <sarkarsoumik215@gmail.com>
2024-07-11 23:50:53 +0600

    fix: bug  on signup controller

--- src/controller/signup.controller.ts
+++ src/controller/signup.controller.ts
@@ -17,14 +17,14 @@ const signUpUser = async (req: Request, res: Response) => {
         const findExistingUser = await User.findOne({ emailOrMobile });
 
         if (findExistingUser) {
-            return res.json({ message: " Your email or number are already used!", success: false, data: null, status: 401 })
+            return res.json({ message: " Your email or number are already used!", success: false, data: null, status: 301 })
         }
         const createUser = await User.create({ firstName, surname, emailOrMobile, password, gender, dateOfBirth })
 
 
         return res.json({ message: "You're account create successfully", status: 200, success: true, data: createUser })
     } catch (error) {
-        return res.json({ message: "Something want wrong", status: false, data: error })
+        return res.json({ message: "Something want wrong", status: 400, data: error, success: false, })
     }
 }
 
